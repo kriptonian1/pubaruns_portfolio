996ea1e245a688025504c76b4f9bbe476d4588bf
darksaiii <matanu743@gmail.com>
2023-01-07 03:39:09 +0530

    Pic & font not fixing-help

--- pages/404.js
+++ pages/404.js
@@ -1,8 +1,22 @@
-export default function Custom404() {
+import Image from 'next/image'
+import { ErrorPic } from '../public/ErrorPic.svg'
+import Link from 'next/link';
+import React from 'react';
+
+const Custom404 = () => {
   return (
-    <div>
-      <h1>
-        404 - Page Not Found</h1>
-    </div>
+    <main class="h-screen w-full flex flex-col justify-center items-center bg-[#DCDCDC]">
+      <h1 class="text-9xl font-extrabold text-[#333333] tracking-widest">
+        Error
+      </h1>
+      <Image src={ErrorPic} alt="hero" className="md:w-[20rem] lg:w-[30vw]" />
+      <Link href='/'>
+        <div className='py-[10px]  text-[#ffffff] transition-all  font-semibold rounded-md bg-[#333333] hover:bg-[#00000046] px-[10px]'>
+            Back to Home Page
+        </div>
+      </Link>
+    </main>
   );
 }
+
+export default Custom404;
