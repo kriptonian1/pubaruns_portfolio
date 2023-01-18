e7720b437fd2e90bff933ac9ca6a4efa5cb91c86
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-18 19:16:52 +0530

    docs: work mockup

---
+++ components/work/data/work.js
@@ -0,0 +1,33 @@
+
+
+const workData = [
+    {
+        id: 1,
+        title: "Work 01",
+        description:
+            "The Youth Photographer of the Year award went to 19-year-old Pubarun Basu from India, for his image No Escape from Reality, which uses cage-like shadows behind curtains to convey a sense of entrapment.",
+        image: "https://i.postimg.cc/52wJwjkF/BBC-Logo.png",
+        alt: "bbc-logo",
+        link: "https://www.bbc.com/news/in-pictures-56719188",
+    },
+    {
+        id: 1,
+        title:
+            "Work 02",
+        description:
+            "Selected from six category winners, Pubarun Basu (India, 19 years-old) has won Youth Photographer of the Year 2021 for his image No Escape from Reality. In the photograph the shadows of railings projected onto curtains create the illusion of cage bars from behind which a pair of hands is seen as if trying to break through. The illusion of shadows and hands gesture convey a sense of entrapment shared by so many across the world this past year. For his win Basu receives Sony’s digital imaging kit and global exposure.",
+        image: "https://i.postimg.cc/Kjh9QmYp/image-18-3.png",
+        alt: "the-telegaph-logo",
+        link: "https://www.telegraph.co.uk/news/2021/04/15/fourteen-spectacular-winning-images-sony-world-photography-awards/",
+    },
+    {
+        id: 1,
+        title: "Work 03",
+        description:
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eu aliquam nisl lorem quis nisl. Sed euismod, nunc ut aliquam aliquam, nisl nisl aliquet nisl, eu aliquam nisl lorem quis nisl.",
+        image: "/images/stock-photos.jpg",
+        link: "https://www.pubarun.com",
+    }
+];
+
+export default workData;
