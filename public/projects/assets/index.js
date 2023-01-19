1fd6a7b9b28a1b052f809192ba8fe10ae18287de
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-19 19:52:02 +0530

    few asserts :)

---
+++ public/projects/assets/index.js
@@ -0,0 +1,25 @@
+import headerPortfolioImg from "./01_The Flow of Civilization.jpg";
+import shortsThumb from "./shortsThumb.png";
+import shortsThumb1 from "./shortsThumb1.png";
+import shortsThumb2 from "./shortsThumb2.png";
+import shortsThumb3 from "./shortsThumb3.png";
+import ExploreImg from "./explore.svg";
+import ViewAll from "./viewAll.svg";
+import projectThumb1 from "./projectThumb1.png";
+import projectThumb from "./projectThumb.png";
+import projectThumb2 from "./projectThumb2.png";
+import projectThumb3 from "./projectThumb3.png";
+
+export {
+  headerPortfolioImg,
+  ExploreImg,
+  shortsThumb,
+  ViewAll,
+  shortsThumb1,
+  shortsThumb2,
+  shortsThumb3,
+  projectThumb,
+  projectThumb1,
+  projectThumb2,
+  projectThumb3,
+};
