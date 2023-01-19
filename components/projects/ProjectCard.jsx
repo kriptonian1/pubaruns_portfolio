eb0b06cb2e7ed34aedc73cd98e6a8953f8e26a0a
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-19 19:51:40 +0530

    improvemnent

--- components/projects/ProjectCard.jsx
+++ components/projects/ProjectCard.jsx
@@ -7,7 +7,7 @@ const ProjectCard = ({ title, description, date, image, slug }) => {
     return (
         <div className="bg-white md:w-[25vw] h-auto  rounded-2xl shadow-none transition duration-500 hover:shadow-xl">
             <Link href={`/project/${slug}`}>
-                <Image className="rounded-t-2xl" src={image} alt="" />
+                <Image className="rounded-t-2xl" src={image} alt="" width={1920} height={1080} />
                 <div className="m-6 ">
                     <div className="font-bold text-2xl pb-4">{title}</div>
                     <div className="font-semibold mt-6">
