06e4cb2eb83d8874b224c1a2824b532f84673b56
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-19 19:39:45 +0530

    navigate link

--- components/projects/ProjectCard.jsx
+++ components/projects/ProjectCard.jsx
@@ -1,20 +1,23 @@
 import Image from 'next/image'
 import React from 'react'
+import Link from 'next/link'
 import { projectThumb } from './assets'
 
-const ProjectCard = ({title, description, date, image}) => {
+const ProjectCard = ({ title, description, date, image, slug }) => {
     return (
         <div className="bg-white md:w-[25vw] h-auto  rounded-2xl shadow-none transition duration-500 hover:shadow-xl">
-            <Image className="rounded-t-2xl" src={image} alt="" />
-            <div className="m-6 ">
-                <div className="font-bold text-2xl pb-4">{title}</div>
-                <div className="font-semibold mt-6">
-                    {description}
+            <Link href={`/project/${slug}`}>
+                <Image className="rounded-t-2xl" src={image} alt="" />
+                <div className="m-6 ">
+                    <div className="font-bold text-2xl pb-4">{title}</div>
+                    <div className="font-semibold mt-6">
+                        {description}
+                    </div>
+                    <div className='text-sm mt-3 pb-6'>
+                        {date}
+                    </div>
                 </div>
-                <div className='text-sm mt-3 pb-6'>
-                   {date}
-                </div>
-            </div>
+            </Link>
         </div>
     )
 }
