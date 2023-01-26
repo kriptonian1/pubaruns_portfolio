f3499c5ede56a21efb0f86cbc655b35005547df5
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-26 19:16:10 +0530

    update WorkSeen.jsx

--- components/home/WorkSeen.jsx
+++ components/home/WorkSeen.jsx
@@ -20,13 +20,13 @@ const WorkSeen = () => {
 
             <div className='flex items-center justify-between'>
                 <HeadLinesHome title={"WORKS SEEN & FEATURED ON"} />
-                <Link className='flex text-sm underline items-center font-semibold hover:-translate-y-1 hover:scale-110 duration-700 transition ease-in-out delay-150' href="/work">
+                <Link className='lg:flex hidden text-sm underline items-center font-semibold hover:-translate-y-1 hover:scale-110 duration-700 transition ease-in-out delay-150' href="/work">
                     VIEW ALL<LinkLogo />
                 </Link>
             </div>
 
 
-            <div className='grid gap-y-9 grid-cols-3 grid-rows-2 mx-[12vw] my-[7vh] items-center'>
+            <div className='grid gap-y-9 gap-2 grid-cols-3 grid-rows-2 mx-[12vw] my-[7vh] items-center'>
                 {work.map((item, index) => {
                     return (
                         <Image draggable={false} key={index} src={item} alt={`${item} Pubarun`} />
