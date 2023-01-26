b6adf7aaf8f0b4b6373497aa85ae5d22c80c7265
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-26 20:12:03 +0530

    update DockerNav.jsx

--- components/common/DockerNav.jsx
+++ components/common/DockerNav.jsx
@@ -22,7 +22,7 @@ const DockerNav = ({ isVisible }) => {
     }
 
     return (
-        <div className='flex justify-center '>
+        <div className='lg:flex hidden justify-center '>
             <nav style={{ visibility: isVisible ? 'visible' : 'hidden' }} className={`fixed z-[999] flex justify-center items-center bottom-9 transition-all duration-200 ${isVisible ? 'opacity-100 visible' : 'hidden opacity-0'}  shadow-2xl flex gap-7 text-[#DCDCDC] bg-[#3e3e3ecc] border-[10px] backdrop-blur-sm md:px-[15px] px-2 md:py-[5px] border-[#0000001a] md:w-[40rem] w-[20rem] md:rounded-[20px] rounded-[20px]`}>
                 <Link href='/'>
                     <div className={`${highlightOnRoute(currrentRoute === "/")} py-[10px] transition-all rounded-md duration-500  font-semibold  px-[10px]`}>
