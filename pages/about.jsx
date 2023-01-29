adcd3d405f86bc6fd7945a8ea1ba3e53cdaa5609
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-01-29 11:58:05 +0530

    update about.jsx

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,19 +1,18 @@
 import Head from "next/head";
 import Image from "next/image";
 import React, { useState } from "react";
-import { ArticleCard, SideSection } from "../components";
 import { DockerNav, Navbar, MobileDockerNav } from "../components/common";
-import { featureData, awardData, workData } from "../components/work/data";
 
 
-function about() {
+
+function About() {
     return (
         <>
             <Navbar isFixed={true} />
-            <div className="flex flex-row justify-between px-10 items-center min-h-screen ">
+            <div className="flex flex-row justify-between px-10 items-center h-[100vh]">
                 <div className="p-10 lg:basis-1/2 basis-1">
-                    <h1 className="font-Montserrat font-bold text-[64px] leading-[78.02px]">PUBARUN BASU</h1>
-                    <p className="pl-5 font-Poppins font-normal text-2xl leading-9">Suspendisse potenti. Nulla facilisi. Praesent vestibulum eros dolor, ut maximus lacus pulvinar in. Nam at pellentesque erat, quis viverra erat.</p>
+                    <h1 className="font-Montserrat font-bold lg:text-[64px] lg:leading-[78.02px]">PUBARUN BASU</h1>
+                    <p className="pl-5 font-Poppins font-normal lg:text-2xl leading-9">Suspendisse potenti. Nulla facilisi. Praesent vestibulum eros dolor, ut maximus lacus pulvinar in. Nam at pellentesque erat, quis viverra erat.</p>
                 </div>
                 <div className="lg:flex hidden justify-center items-center">
                     <Image src={'/PUBARUN BASU.png'} alt="PUBARUN BASU" width={500} height={500} />
@@ -26,4 +25,4 @@ function about() {
     )
 }
 
-export default about
+export default About
