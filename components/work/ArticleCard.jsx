6258eefbfcfc289aba0bc0248fa5d74e79eeedd3
Abhishek kushwaha <abhishekkushwaha1479@gmail.com>
2023-02-02 14:49:52 +0530

    Work Section Mobile Responsive init

--- components/work/ArticleCard.jsx
+++ components/work/ArticleCard.jsx
@@ -8,13 +8,13 @@ const ArticleCard = ({ title, description, link, logo, alt }) => {
 
         <div className="m-7 border-b border-black pb-7 border-dashed ">
             <div className="flex flex-col space-y-[2rem]">
-                <div className="flex justify-between items-center">
+                <div className="flex justify-between items-center gap-5">
                     <a href={link} target="_blank" rel="noopener noreferrer">
                         <h1 className="text-lg font-medium underline max-w-sm">
                             {title}
                         </h1>
                     </a>
-                    <Image draggable={false} src={logo} alt={alt} className=' sm:w-16 md:w-28 lg:w-36 h-auto' width={200} height={100} />
+                    <Image draggable={false} src={logo} alt={alt} className='w-24 md:w-28 lg:w-36 h-auto' width={200} height={100} />
                 </div>
                 <div className="flex lg:flex-row flex-col lg:justify-between lg:items-end gap-2">
                     <p className=" lg:max-w-[35vw]">
