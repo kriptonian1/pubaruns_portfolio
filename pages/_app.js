cfdff9960c812c7915a074c04c05bf1a1de3e690
kriptonian <shymal.sawan@gmail.com>
2023-02-04 00:49:32 +0530

    feat: add vercel analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,18 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 // disable console.log in production
-if (process.env.NODE_ENV === 'production') {
-  console.log = () => {}
+if (process.env.NODE_ENV === "production") {
+  console.log = () => {};
 }
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Component {...pageProps} />
+      <Analytics />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
